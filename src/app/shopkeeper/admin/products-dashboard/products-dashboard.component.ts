38eccfc35303275d96da9a467af95e4bd16a516a
Victor <victoralves28.pg@gmail.com>
2017-11-15 22:13:34 -0200

    PRODUCTS DASHBOARD - MATERIAL DESIGN

--- src/app/shopkeeper/admin/products-dashboard/products-dashboard.component.ts
+++ src/app/shopkeeper/admin/products-dashboard/products-dashboard.component.ts
@@ -4,11 +4,13 @@ import { ProductsService } from './services/products.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 import { User } from 'firebase/app';
-import { PaginationInstance } from 'ngx-pagination';
 
 import { Subject } from 'rxjs/Subject';
 import { ViewContainerRef } from '@angular/core';
 import { TdDialogService } from '@covalent/core';
+import { Router } from '@angular/router';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-products-dashboard',
@@ -18,33 +20,19 @@ import { TdDialogService } from '@covalent/core';
 export class ProductsDashboardComponent {
 
   stores: any[];
-  products;
   user: User;
-  query: any;
-  currentPage = 1;
   userSubscription;
-  productsSubscription;
   lastSelected;
   products$ = new Subject<string>();
-  public paginationComponentConfig: PaginationInstance = {
-    id: 'products-pagination',
-    itemsPerPage: 10,
-    currentPage: 1
-  };
+  displayedColumns = ['name', 'price', 'upVotes', 'downVotes', 'actions'];
+  dataSource: ProductsDataSource;
 
   constructor(private productsService: ProductsService, private afAuth: AngularFireAuth, private dialogService: TdDialogService,
-    private viewContainerRef: ViewContainerRef) {
+    private viewContainerRef: ViewContainerRef, private router: Router) {
 
     this.products$.asObservable().subscribe((storeId) => {
-      this.products = this.productsService.getProductsFrom(storeId);
-      this.productsSubscription = this.products.subscribe();
+      this.dataSource = new ProductsDataSource(productsService, storeId);
     });
-
-    /*  this.userSubscription = productsService.getUser().subscribe((user: any) => {
-        this.stores = user.worksAt;
-        this.lastSelected = this.stores[0].storeId;
-        this.products$.next(this.lastSelected);
-      });*/
 
     this.userSubscription = productsService.getStoresWhereUserWorks().subscribe((stores) => {
       this.stores = stores;
@@ -56,9 +44,7 @@ export class ProductsDashboardComponent {
     this.afAuth.auth.onAuthStateChanged((user) => {
       if (!user) {
         console.log('destrói tuto chessus');
-        this.products.subscribe().unsubscribe();
         this.userSubscription.unsubscribe();
-        this.productsSubscription.unsubscribe();
         this.products$.unsubscribe();
       }
     });
@@ -66,8 +52,6 @@ export class ProductsDashboardComponent {
 
   onChange(value) {
     this.lastSelected = value;
-    this.query = '';
-
     this.products$.next(this.lastSelected);
   }
 
@@ -84,6 +68,22 @@ export class ProductsDashboardComponent {
         this.productsService.deleteProduct(key, categories, store, picsQty);
       }
     });
-
   }
+
+  updateProduct(key) {
+    this.router.navigate([`/shopkeeper/dashboard/admin/products/edit/${key}`]);
+  }
+}
+
+export class ProductsDataSource extends DataSource<any> {
+
+  constructor(private productsService: ProductsService, private storeId) {
+    super();
+  }
+
+  connect(): Observable<any[]> {
+    return this.productsService.getProductsFrom(this.storeId);
+  }
+
+  disconnect() { }
 }
