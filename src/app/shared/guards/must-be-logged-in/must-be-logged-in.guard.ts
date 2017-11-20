5c7f8d64e048565341533915afab24aeadbc3aae
Victor <victoralves28.pg@gmail.com>
2017-11-20 14:23:56 -0200

    REFATORAÇÃO DE TELAS | SISTEMA DE AUTENTICAÇÃO MELHORADO

--- src/app/shared/guards/must-be-logged-in/must-be-logged-in.guard.ts
+++ src/app/shared/guards/must-be-logged-in/must-be-logged-in.guard.ts
@@ -9,11 +9,11 @@ import { environment } from '../../../../environments/environment';
 @Injectable()
 export class MustBeLoggedInGuard implements CanActivate {
 
-    constructor(private router: Router, private authService: AuthService, private db: AngularFireDatabase ) {}
+    constructor(private router: Router, private authService: AuthService, private db: AngularFireDatabase) { }
 
     canActivate(
         next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot ): Observable<boolean> | boolean {
+        state: RouterStateSnapshot): Observable<boolean> | boolean {
         const user = JSON.parse(localStorage.getItem(`firebase:authUser:${environment.firebase.apiKey}:[DEFAULT]`));
         console.log(user);
         let canActivate;
@@ -24,13 +24,13 @@ export class MustBeLoggedInGuard implements CanActivate {
                     canActivate = currentUser.emailVerified && currentUser.profileVerified;
                     if (!canActivate) {
                         console.log('can t');
-if(!currentUser.emailVerified) {
- this.router.navigate(['/subscribe/verification/email']);
-} else if(!currentUser.profileVerified) {
- this.router.navigate(['/subscribe/verification/admin']);
-} else {
-                        this.router.navigate(['/subscribe']);
-}
+                        if (!currentUser.emailVerified) {
+                            this.router.navigate(['/subscribe/verification/email']);
+                        } else if (!currentUser.profileVerified) {
+                            this.router.navigate(['/subscribe/verification/admin']);
+                        } else {
+                            this.router.navigate(['/subscribe']);
+                        }
                         return false;
                     }
                     console.log(canActivate);
